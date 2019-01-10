005ce410e59cd364a5f5520320b772d932d2eaa4
Rudi Wijaya <rudi.mail90@gmail.com>
2019-01-10 08:56:12 +0700

    load from file properties

--- rms-servlet-web/src/main/java/com/mitrais/rms/dao/DataSourceFactory.java
+++ rms-servlet-web/src/main/java/com/mitrais/rms/dao/DataSourceFactory.java
@@ -3,8 +3,12 @@ package com.mitrais.rms.dao;
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 import javax.sql.DataSource;
+import javax.swing.event.InternalFrameEvent;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * This class provides MySQL datasource to be used to connect to database.
@@ -16,11 +20,21 @@ public class DataSourceFactory {
     DataSourceFactory() {
         final MysqlDataSource dataSource = new MysqlDataSource();
         // TODO: make these database setting configurable by moving to properties file
-        dataSource.setDatabaseName("rmsdb");
-        dataSource.setServerName("127.0.0.1");
-        dataSource.setPort(3306);
-        dataSource.setUser("root");
-        dataSource.setPassword("mitrais");
+
+        final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        final InputStream input = classLoader.getResourceAsStream("database.properties");
+        try {
+            final Properties prop = new Properties();
+            prop.load(input);
+
+            dataSource.setDatabaseName(prop.getProperty("jdbc.db"));
+            dataSource.setServerName(prop.getProperty("jdbc.host"));
+            dataSource.setPort(Integer.valueOf(prop.getProperty("jdbc.port")));
+            dataSource.setUser(prop.getProperty("jdbc.user"));
+            dataSource.setPassword(prop.getProperty("jdbc.password"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         this.dataSource = dataSource;
     }
 
